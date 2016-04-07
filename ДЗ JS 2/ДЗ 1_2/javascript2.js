8405bca2a6c3f29d890179ac8ccec5556b687f74
ViktorMn <m.85viktor@gmail.com>
2016-04-07 13:08:28 +0400

    rectified ДЗ 1_2

--- ДЗ JS 2/ДЗ 1_2/javascript2.js
+++ ДЗ JS 2/ДЗ 1_2/javascript2.js
@@ -1,14 +1,8 @@
 
-// POW
-// function equal(x,y) {
-//Функция возведения в степень
- // Math.pow(x,y);
-
-// }
  function pow(x, y) {
   var result = x;
 
-  for (var i = 1; i < n; i++) {
+  for (var i = 1; i < y; i++) {
     result *= x;
   }
 
@@ -21,7 +15,8 @@ var y = prompt("Введите степень", '');
 if (y <= 1) {
   alert('Степень ' + y + 'не поддерживается, введите целую степень, больше чем 1'
   );
-} else {
+} 
+  else {
   alert( pow(x, y) );
 }
   
